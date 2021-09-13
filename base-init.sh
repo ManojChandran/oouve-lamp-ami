4c589eaa70cc34e45fff73b1b4b9aaecc996d90e
Manojchandran <manoj.meparambu@gmail.com>
2021-09-13 20:15:41 +0530

    updated readme

--- base-init.sh
+++ base-init.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
 echo -e "\n\nUpdating Apt Packages and upgrading latest patches\n"
-sudo apt-get update -y && sudo apt-get upgrade -y
+sudo yum update -y
 
 echo -e "\n\nInstalling Apache2 Web server\n"
-sudo apt-get install apache2 apache2-doc apache2-mpm-prefork apache2-utils libexpat1 ssl-cert -y
+sudo yum install apache2 apache2-doc apache2-mpm-prefork apache2-utils libexpat1 ssl-cert -y
 
 echo -e "\n\nInstalling PHP & Requirements\n"
-sudo apt-get install libapache2-mod-php7.0 php7.0 php7.0-common php7.0-curl php7.0-dev php7.0-gd php-pear php7.0-mcrypt php7.0-mysql -y
+sudo yum install libapache2-mod-php7.0 php7.0 php7.0-common php7.0-curl php7.0-dev php7.0-gd php-pear php7.0-mcrypt php7.0-mysql -y
 
 echo -e "\n\nInstalling MySQL\n"
-sudo apt-get install mysql-server mysql-client -y
+sudo yum install mysql-server mysql-client -y
 
 echo -e "\n\nPermissions for /var/www\n"
 sudo chown -R www-data:www-data /var/www
 echo -e "\n\n Permissions have been set\n"
 
 echo -e "\n\nEnabling Modules\n"
-sudo a2enmod rewrite
-sudo phpenmod mcrypt
+#sudo a2enmod rewrite
+#sudo phpenmod mcrypt
 
 echo -e "\n\nLAMP Installation Completed"
 
